26d0e9f29d13ea60693fbfdc0158451fe5e91389
sm4rtz-exe <thanawat7cg@gmail.com>
2026-08-20 16:58:33 +0700

    TextField Border

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 1,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+      borderRadius: const BorderRadius.all(Radius.circular(25)),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text(
+          "Currency Converter",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            TextField(
+              style: const TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: Colors.black),
+                hint: const Text("Type your number here"),
+                prefixIcon: const Icon(Icons.monetization_on_outlined),
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
